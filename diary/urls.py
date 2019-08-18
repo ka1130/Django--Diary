b2a17cf8efa26aeab08821ac7052f3d3318790e0
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-08-18 23:03:41 +0200

    Rename blog app to posts

--- diary/urls.py
+++ diary/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-import blog.views
+import posts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.views.home, name='home')
+    path('', posts.views.home, name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
