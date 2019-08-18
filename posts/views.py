b9ec0fd768eeec6a2407faa3f3a3e0c05a546c54
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-08-18 23:12:17 +0200

    Display post data on the home page

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/home.html', {'posts': posts})
