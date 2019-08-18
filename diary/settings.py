036c0124a87f3de2034c09fea88c2f2675eaedaa
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-08-18 22:11:06 +0200

    Add base template

--- diary/settings.py
+++ diary/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'diary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['diary/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- diary/urls.py
+++ diary/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
