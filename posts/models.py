0bd9979a09abe06ee477cce82563b6e51ecf6e0c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-08-28 23:23:34 +0200

    Add default image to the post

--- posts/models.py
+++ posts/models.py
@@ -31,6 +31,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    image = models.ImageField(
+        upload_to='images/', default='media/images/placeholder.png')
 
     def __str__(self):
         return self.title
