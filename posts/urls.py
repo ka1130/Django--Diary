391f76dd21dd2ead55ac22aee502f4f3784afdfc
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-08-31 22:20:14 +0200

    Add basic post detail view

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import (
     PostsListView,
-
+    PostDetailView
 )
 
 urlpatterns = [
-    path('', PostsListView.as_view(), name='posts-list')
+    path('', PostsListView.as_view(), name='posts-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail')
 ]
