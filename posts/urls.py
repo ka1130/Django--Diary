4ceaccf57489fafe4424ae7b0a3f357e315a612e
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-09-05 22:00:21 +0200

    Fix styles

--- posts/urls.py
+++ posts/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('create/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
