0afa3f859d02a6c476525134ab960c043bbb3e6a
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-09-09 22:26:10 +0200

    Add profile view

--- posts/urls.py
+++ posts/urls.py
@@ -7,11 +7,13 @@ from .views import (
     PostUpdateView,
     PostDeleteView,
     register,
+    profile,
 )
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='posts-list'),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
--- posts/views.py
+++ posts/views.py
@@ -20,11 +20,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('posts-list')
+            messages.success(
+                request, f'Your account has been created. You can now login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
 class PostForm(forms.ModelForm):
